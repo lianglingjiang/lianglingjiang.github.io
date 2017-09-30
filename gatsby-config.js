d95f91dbe0a85f5876de1184c5f202cbe73cea5c
lianglingjiang <lianglj@yahoo.com>
2017-09-30 09:32:49 +0800

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Oliver Benns',
+    url: 'https://oliverbenns.com',
+    description: 'Oliver Benns is a freelance web developer working with individuals and agencies across the world.',
+  },
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'UA-42854706-1',
+      },
+    },
+  ],
+};
