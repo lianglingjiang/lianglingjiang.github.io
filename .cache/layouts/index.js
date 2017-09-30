fd6889491dc092396ae891d356e182d61750457a
lianglingjiang <lianglj@yahoo.com>
2017-09-30 14:58:43 +0800

    update

--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "/Users/lianglingjiang/Documents/Devspace/projects/mywebsite/src/layouts/index.jsx"
-  import data from "/Users/lianglingjiang/Documents/Devspace/projects/mywebsite/.cache/json/layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
